9ff7ed2139d6ca4f11512250e519c6c731e20c3d
MinhAnh-haui <luongminhanh.haui@gmail.com>
2023-02-08 16:30:13 +0700

    add b5

---
+++ Bài tập học kết hợp/Bài 5/2021600336_LuongMinhAnh_Bai5_Bai11.sql
@@ -0,0 +1,71 @@
+﻿create database QLKHO
+go
+use QLKHO
+go
+create table Nhap (
+	SoHDN nchar(10),
+	MaVT nchar(10),
+	SoLuongN int,
+	DonGiaN money,
+	NgayN date
+)
+create table Xuat (
+	SoHDX nchar(10),
+	MaVT nchar(10),
+	SoLuongX int,
+	DonGiaX money,
+	NgayX date
+)
+create table Ton (
+	MaVT nchar(10),
+	TenVT nvarchar(20),
+	SoLuongT int
+)
+
+insert into Nhap
+values ('N001', 'VT002', 20, 230, '2021-02-21'),
+('N002', 'VT002', 25, 200, '2021-03-31'),
+('N003', 'VT005', 2, 210, '2021-02-22')
+
+insert into Xuat 
+values ('X001', 'VT002', 17, 230, '2021-02-21'),
+('X002', 'VT003', 20, 200, '2021-03-31')
+
+insert into Ton 
+values ('VT001', 'Xe may', 2),
+('VT002', 'Xe dap', 1),
+('VT003', 'Thuyen', 1),
+('VT004', 'Tau', 1),
+('VT005', 'O to', 1)
+
+--Thống kê tiền bán theo mã vật tư gồm MaVT, TenVT, TienBan (TienBan=SoLuongX*DonGiaX) 
+CREATE VIEW CAU2
+as
+select Xuat.MaVT, TenVT, sum(SoLuongX*DonGiaX) as 'TienBan'
+from Ton inner join Xuat on Ton.MaVT = Xuat.MaVT
+group by Xuat.MaVT, TenVT
+
+--Thống kê soluongxuat theo tên vattu
+create view CAU3
+as 
+select TenVT, sum(SoLuongX) as 'TongLuongXuat'
+from Xuat inner join Ton on Xuat.MaVT = Ton.MaVT
+group by TenVT
+
+--Thống kê soluongnhap theo tên vật tư
+create view CAU4
+as 
+select TenVT, sum(SoLuongN) as 'TongLuongNhap'
+from Nhap inner join Ton on Nhap.MaVT = Ton.MaVT
+group by TenVT
+
+--Đưa ra tổng soluong còn trong kho biết còn = nhap – xuất + tồn theo từng 
+--nhóm vật tư
+
+create view CAU5
+as
+select TenVT, sum(SoluongN-SoluongX+SoLuongT) as 'SoLuong'
+from Nhap inner join Xuat on Nhap.MaVT = Xuat.MaVT
+inner join Ton on Nhap.MaVT = Ton.MaVT
+group by TenVT
+
---
+++ Bài tập học kết hợp/Bài 5/2021600336_LuongMinhAnh_Bai5_Bai22.sql
@@ -0,0 +1,116 @@
+﻿--1. Viết hàm thống kê xem mỗi lớp có bao nhiêu sinh viên với malop là tham số truyền vào 
+--từ bàn phím.
+Create function thong_ke(@malop nchar(10))
+returns int
+as
+begin
+	declare @sl int
+	set @sl = (select count(MaSV)
+	from SV, LOP
+	where SV.MaLop = LOP.MaLop and SV.MaLop = @malop
+	group by TenLop)
+	return @sl
+end
+
+select dbo.thong_ke('L02')
+
+--2. Đưa ra danh sách sinh viên(masv,tensv) học lớp với tenlop được truyền vào từ bàn phím.
+create function fn_timSV(@tenlop nvarchar(10))
+returns @bang table(
+				MaSV nchar(10),
+				TenSV nvarchar(30)
+				)
+as
+begin
+	insert into @bang 
+					select MaSV, TenSV 
+					from SV inner join LOP on SV.MaLop = LOP.MaLop
+					where TenLop = @tenlop
+	return 
+end
+
+select * from dbo.fn_timSV('12A')
+
+--3. Đưa ra hàm thống kê sinhvien: malop,tenlop,soluong sinh viên trong lớp, với tên lớp 
+--được nhập từ bàn phím. Nếu lớp đó chưa tồn tại thì thống kê tất cả các lớp, ngược lại nếu 
+--lớp đó đã tồn tại thì chỉ thống kê mỗi lớp đó.
+alter function fn_tkSV(@tenlop nvarchar(10))
+returns @bang table(
+					malop nchar(10),
+					tenlop nvarchar(10),
+					soluong int
+					)
+as
+begin
+	if (not exists (select TenLop from LOP where TenLop = @tenlop)) 
+	insert into @bang 
+					select SV.MaLop, TenLop, count(MaSV)
+					from SV inner join LOP on SV.MaLop = LOP.MaLop
+					group by SV.MaLop, TenLop
+	else
+	insert into @bang 
+					select SV.MaLop, TenLop, count(MaSV)
+					from SV inner join LOP on SV.MaLop = LOP.MaLop
+					where TenLop = @tenlop
+					group by SV.MaLop, TenLop
+	return
+end
+
+select * from dbo.fn_tkSV('12C')
+
+--4. Đưa ra phòng học của tên sinh viên nhập từ bàn phím.
+create function fn_phong(@tensv nvarchar(30))
+returns nvarchar(20)
+as
+begin
+	declare @phong nvarchar(20)
+	set @phong = (select Phong from LOP
+					inner join SV on LOP.MaLop = SV.MaLop
+					where TenSV = @tensv)
+	return @phong
+end
+
+select dbo.fn_phong('Nguyen Van A')
+
+--5. Đưa ra thống kê masv,tensv, tenlop với tham biến nhập từ bàn phím là phòng. Nếu phòng 
+--không tồn tại thì đưa ra tất cả các sinh viên và các phòng. Neu phòng tồn tại thì đưa ra các 
+--sinh vien của các lớp học phòng đó (Nhiều lớp học cùng phòng).
+alter function fn_inforSV(@phong nvarchar(20))
+returns @bang table (
+					masv nchar(10),
+					tensv nvarchar(30),
+					tenlop nvarchar(10)
+					)
+as
+begin
+	if (not exists(select Phong from Lop where Phong = @phong))
+		insert into @bang 
+				select MaSV, TenSV, TenLop from SV 
+				inner join LOP on SV.MaLop = LOP.MaLop
+	else 
+		insert into @bang 
+				select MaSV, TenSV, TenLop from SV 
+				inner join LOP on SV.MaLop = LOP.MaLop
+				where Phong = @phong
+	return
+end
+
+select * from dbo.fn_inforSV('P11')
+
+
+--6. Viết hàm thống kê xem mỗi phòng có bao nhiêu lớp học. Nếu phòng không tồn tại trả
+--về giá trị 0. 
+create function fn_lop(@phong nvarchar(20))
+returns int 
+as
+begin
+	declare @sl int
+	if (exists (select Phong from LOP where Phong = @phong))
+		set @sl = (select count(MaLop) from LOP
+					where Phong = @phong
+					group by Phong)
+	else set @sl = 0
+	return @sl
+end
+
+select dbo.fn_lop('P01')
